3bfcb18f2b10afd9b9d9fa5ca1040e0df972bcb8
Vince Veselosky <vince@control-escape.com>
2012-09-02 09:17:56 -0400

    upgrade to otto 0.4

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,13 @@
 from fabric.api import env, lcd, local, task
-import os.path
 import otto.web as web
+from otto.util import paths
 
 env['otto.web.build_dir'] = './build/staged'
 env['otto.web.site'] = 'veselosky.me'
-
-def _project_dir():
-    """Helper to return the project directory"""
-    return os.path.dirname(os.path.realpath(env['real_fabfile']))
+# For otto >= 0.3
+env['otto.build_dir'] = 'build/staged'
+env['otto.site'] = 'veselosky.me'
+env['otto.home'] = '/home/vince/websites/'
 
 @task
 def B():
@@ -18,13 +18,15 @@ def B():
 def build():
     """Build the site"""
     target = env['otto.web.build_dir']
-    with lcd(_project_dir()):
+    with lcd(paths.local_workspace()):
         local('mkdir -p %s' % target)
         local('cp -a etc %s/' % target)
 
 @task
 def clean():
     """Clean up stale build files and tarballs"""
-    with lcd(_project_dir()):
-        local('rm -rf build/*') # make clean
-        local('rm -f *.tar.gz') # make clean
+    with lcd(paths.local_workspace()):
+        local('rm -rf build/*')
+        local('rm -f *.tar.gz')
+        local("find . -name '*.py[co]' | xargs rm")
+
