02dbde454c465d03b5babc291d50d1a723c8281a
Vince Veselosky <vince@control-escape.com>
2019-05-04 18:28:53 -0400

    Factor out metadata processing

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,8 @@
  */
 const path = require("path")
 const _ = require("lodash")
-const moment = require("moment")
+// const moment = require("moment")
+const webquills = require("./lib/webquills.js")
 //const siteConfig = require("./data/SiteConfig")
 
 /**
@@ -14,76 +15,15 @@ const moment = require("moment")
  */
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  let category, date, description, itemtype, slug, tags, title
 
   // Only care about markdown nodes (articles)
   if (node.internal.type === "MarkdownRemark") {
-    const fileNode = getNode(node.parent)
-    const parsedFilePath = path.parse(fileNode.relativePath)
-
-    // If we have frontmatter, interrogate it. NOTE: Webquills allows title
-    // case metadata, check for both.
-    if ("frontmatter" in node) {
-      // Lift title & description.
-      title = node.frontmatter.title || node.frontmatter.Title
-      description = node.frontmatter.description || node.frontmatter.Description
-      itemtype = node.frontmatter.itemtype
-
-      // Lift category.
-      category = node.frontmatter.category || node.frontmatter.Category
-
-      // Lift tags
-      tags = node.frontmatter.tags || node.frontmatter.Tags || []
-
-      const dateStr =
-        node.frontmatter.date ||
-        node.frontmatter.Date ||
-        node.frontmatter.updated ||
-        node.frontmatter.Updated ||
-        node.frontmatter.published ||
-        node.frontmatter.Published
-
-      date = moment(dateStr)
-      // if (!date.isValid) console.warn(`WARNING: Invalid date.`, dateStr)
-    } // if frontmatter
-
-    /**
-     * Now set values for fields not found in frontmatter
-     */
-    // TODO Pull the create/modify date from the file node.
-    if (!date || !date.isValid) date = moment()
-    if (!description) description = node.excerpt
-
-    if (!title && "headings" in node && node.headings.length > 0) {
-      const headline = node.headings.find(x => (x.depth === 1 ? true : false))
-      if (headline) title = headline.value
+    console.info("MarkdownRemark node")
+    let meta = webquills.extractRemarkMeta(node, getNode)
+    console.info(meta)
+    for (let key in meta) {
+      createNodeField({ node, name: key, value: meta[key] })
     }
-    // Generate slug without title
-    if (!slug) {
-      if (parsedFilePath.name !== "index" && parsedFilePath.dir !== "") {
-        if (!category) category = _.startCase(parsedFilePath.dir)
-        slug = `/${parsedFilePath.dir}/${parsedFilePath.name}`
-      } else if (parsedFilePath.dir === "") {
-        slug = `/${parsedFilePath.name}`
-      } else {
-        slug = `/${parsedFilePath.dir}/`
-      }
-    }
-    // Append .html extension unless the slug ends in /
-    if (!slug.endsWith("/")) slug += ".html"
-
-    if (!tags) tags = ["_UNTAGGED"]
-
-    createNodeField({ node, name: "category", value: category })
-    createNodeField({ node, name: "date", value: date.toISOString() })
-    createNodeField({ node, name: "description", value: description })
-    createNodeField({ node, name: "itemtype", value: itemtype })
-    createNodeField({ node, name: "slug", value: slug })
-    // FIXME After adding tags here, query results in error below. WTF? VV 2019-04-16
-    // GraphQLError: Cannot query field "tags" on type "MarkdownRemarkFields".
-    // Not actually using tags in practice (yet) so I removed that from the query for now.
-    createNodeField({ node, name: "tags", value: tags })
-    createNodeField({ node, name: "title", value: title })
   } // if markdown
 } // onCreateNode
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,10 +9,9 @@ import TeaseList from "../components/teaseList"
 
 const IndexPage = props => {
   const { data } = props
-  const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
   return (
-    <Layout location={props.location} title={siteTitle}>
+    <Layout>
       <SEO title="All posts" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
       <TeaseList pages={posts} />
     </Layout>
@@ -20,7 +19,7 @@ const IndexPage = props => {
 }
 
 IndexPage.propTypes = {
-  location: PropTypes.string,
+  location: PropTypes.object,
   data: PropTypes.object,
 }
 
@@ -28,11 +27,6 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(
       filter: { fields: { itemtype: { eq: "Item/Page/Article" } } }
       sort: { fields: [fields___date], order: DESC }
